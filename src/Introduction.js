156bf7bb3ecdce18fd7f4aa8ea294c6abe043342
Casaemiro <silasmagho@gmail.com>
2022-06-16 08:40:12 +0100

    protfolio

---
+++ src/Introduction.js
@@ -0,0 +1,25 @@
+import React from 'react';
+const Introduction = () => {
+    return ( 
+        <div className="intro">
+            <div className="intro-image">
+                <img src="./images/intro-img.jpg" alt=""></img>
+            </div>
+            <div className="intro-text">
+                <h1>Software Engineer</h1>
+                <p className="intro-details">
+                I am a young smart and open minded individual
+passionate about solving problems. I love
+environments and scenarios that allow me to think
+and solve problems. I’m also passionate about
+teaching which I am excellent at and I find pleasure
+in helping others understand. I don’t see myself as
+the best engineer but I am always willing and ready
+to learn.
+                </p>
+            </div>
+        </div>
+     );
+}
+ 
+export default Introduction;
---
+++ src/Navbar.js
@@ -0,0 +1,16 @@
+import "./index.css";
+export const Navbar = () => {
+    return ( 
+        <div className="top-nav">
+            <h1>A.Silas Magho</h1>
+            <div className="links">
+                <a href="iknite.space">about me</a>
+                <a href="iknite.space">resume</a>
+                <a href="iknite.space">portfolio</a>
+                <a href="iknite.space">help</a>
+                <a href="iknite.space">GitHub</a>
+            </div>
+        </div>
+     );
+}
+ 
